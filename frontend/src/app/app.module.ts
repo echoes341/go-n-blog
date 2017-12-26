341c1da7626b06d5d92930d23382376740998493
G. R <gpl.rgp.pgl@gmail.com>
2017-12-27 00:11:19 +0100

    Add add-comment component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { Routes, RouterModule, ActivatedRoute } from '@angular/router';
 import { ViewArticleComponent } from './article/view-article/view-article.component';
 import { ArticleContainerComponent } from './article/article-container/article-container.component';
 import { CommentViewComponent } from './comments/comment-view/comment-view.component';
+import { AddCommentComponent } from './comments/add-comment/add-comment.component';
 
 const appRoutes: Routes = [
   {path: '', component: ArticleListComponent},
@@ -32,7 +33,8 @@ const appRoutes: Routes = [
     ArtbydateRecapComponent,
     ViewArticleComponent,
     ArticleContainerComponent,
-    CommentViewComponent
+    CommentViewComponent,
+    AddCommentComponent
   ],
   imports: [
     BrowserModule,
