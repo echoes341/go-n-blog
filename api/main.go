cee9be7ded747ea1ea788e9505ee8584dda4f608
G. R <gpl.rgp.pgl@gmail.com>
2018-01-30 00:25:14 +0100

    Fix db automigrate

--- api/main.go
+++ api/main.go
@@ -7,24 +7,15 @@ import (
 	"github.com/dimfeld/httptreemux"
 	"github.com/echoes341/go-n-blog/api/cache"
 	"github.com/echoes341/go-n-blog/api/models"
-
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
-
-var db *gorm.DB
 
 func init() {
 	// open db connection
 	var err error
-	db, err = models.NewDB("gonblog", "gonblog", "127.0.0.1:3306", "gonblog")
+	err = models.NewDB("gonblog", "gonblog", "127.0.0.1:3306", "gonblog")
 	if err != nil {
 		log.Panicln("Database connection failed")
 	}
-
-	db.AutoMigrate(&userDB{})
-	db.AutoMigrate(&commentDB{})
-	db.AutoMigrate(&likeDB{})
 }
 
 func main() {
--- api/models/models.go
+++ api/models/models.go
@@ -8,6 +8,8 @@ import (
 	"log"
 
 	"github.com/jinzhu/gorm"
+	// mysql adapter
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var db *gorm.DB
@@ -17,14 +19,17 @@ var db *gorm.DB
 // - p is the password
 // - a is the db address
 // - d is the db name
-func NewDB(u, p, a, d string) (*gorm.DB, error) {
+func NewDB(u, p, a, d string) error {
 	var err error
 	par := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", u, p, a, d)
 	log.Println(par)
 	db, err = gorm.Open("mysql", par)
-	// automigrate should be here
+	// automigrate dbModels
 	db.AutoMigrate(&articleDB{})
-	return db, err
+	db.AutoMigrate(&userDB{})
+	db.AutoMigrate(&commentDB{})
+	db.AutoMigrate(&likeDB{})
+	return err
 }
 
 // Close closes the database
