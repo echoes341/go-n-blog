ea36b8c8393b10f20109886a6d95dd6b47005df0
G. R <gpl.rgp.pgl@gmail.com>
2018-01-30 20:25:09 +0100

    Idiomatic interface

--- api/middleware.go
+++ api/middleware.go
@@ -4,22 +4,23 @@ import (
 	"net/http"
 )
 
-// useGET: generic middleware handler for GET methods
-type getInterface interface {
+// Getter interface containing the GET method
+type Getter interface {
 	GET(string, http.HandlerFunc)
 }
 
 type middleFunc func(http.HandlerFunc) http.HandlerFunc
 
-func useGET(r getInterface, fn middleFunc) *middleWare {
+// useGET: generic middleware handler for GET methods
+func useGET(r Getter, fn middleFunc) *middleWare {
 	return &middleWare{fn, r}
 }
 
 type middleWare struct {
 	fn middleFunc
-	getInterface
+	Getter
 }
 
 func (m *middleWare) GET(path string, fn http.HandlerFunc) {
-	m.getInterface.GET(path, m.fn(fn))
+	m.Getter.GET(path, m.fn(fn))
 }
