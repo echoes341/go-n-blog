7d5c8a0d428857c03289125d23ae93a846c6095b
G. Rossin <gpl.rgp.pgl@gmail.com>
2018-11-27 00:30:47 +0100

    Remove frontend directory

--- frontend/src/app/article/like.model.ts
+++
@@ -1,7 +0,0 @@
-export class Like {
-  constructor(
-    public id: number, // Like ID
-    public idA: number, // Article ID
-    public idU: number, // User's like ID
-  ) { }
-}
--- frontend/src/app/comments/comment.model.ts
+++
@@ -1,9 +0,0 @@
-export class Comment {
-  constructor(
-    public id: number, // comment's own id
-    public idA: number, // id of the article which the comment is related to
-    public name: string, // name of the author
-    public email: string, // email of the autorh
-    public content: string // text content of the comment
-  ) { }
-}
